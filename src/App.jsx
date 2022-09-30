b5fd1ddbf3d2ae4be39b467ab2ad141367259d69
ShuumaIwamoto <iwa06ki12da@gmail.com>
2022-10-01 00:05:40 +0900

    Sidebarの雛形。 insert押下反応test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  //複数の追加Noteを格納する配列
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: 1,
+      title: "NewNote!",
+      content: "NewNoteSample",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Sidebar1.css";
-const Sidebar = () => {
+
+const Sidebar = ({ onAddNote }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>note</h1>
-        <button>insert</button>
+        <button onClick={onAddNote}>insert</button>
       </div>
       <div className="app-sidebar-notes">
         <div className="app-sidebar-note">
@@ -13,7 +14,7 @@ const Sidebar = () => {
             <strong>title</strong>
             <button>delete</button>
           </div>
-          <p>noteの内容</p>
+          <p>ノートの内容です</p>
           <small>最後の修正日xx/xx</small>
         </div>
       </div>
